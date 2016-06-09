67108e1f24ce1d4d2433a3ae038d1733c91e88ad
owen <stensonowen@gmail.com>
2016-06-08 20:08:43 -0400

    fixed collision detection & cleaned up

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -5,6 +5,7 @@ pub mod block;
 pub mod board {
     extern crate rand;
     extern crate std;
+    extern crate ncurses;
     use board::block::cell::*;
     use board::block::*;
     use std::fmt;
@@ -27,10 +28,11 @@ pub mod board {
             let mut copy = self.clone();
             if let Some(piece) = copy.block {
                 copy.incorporate(&piece);
-            } else {
-                println!("failed to incorp");
             }
-            try!(write!(f, "\n\t"));
+            let horiz: String = std::iter::repeat("-")
+                .take(WIDTH+1).collect();
+            //try!(write!(f, "\n\t"));
+            try!(write!(f, "\n\t{}|\n\t|", horiz.clone()));
             for i in copy.table.iter() {
                 for j in i {
                     match *j {
@@ -38,9 +40,9 @@ pub mod board {
                         None    => try!(write!(f, " ")),
                     };
                 }
-                try!(write!(f, "\n\t"));
+                try!(write!(f, "|\n\t|"));
             }
-            writeln!(f, "")
+            writeln!(f, "{}", horiz)
         }
     }
 
@@ -52,14 +54,14 @@ pub mod board {
                 block: None,
             }
         }
-        fn get(&self, x:usize, y:usize) -> Option<Cell> {
+        pub fn get(&self, x:usize, y:usize) -> Option<Cell> {
             self.table[y][x]
         }
         pub fn new_piece(&mut self) {
             assert!(self.block.is_none());
             self.block = Some(Piece::new(
                     WIDTH/2-1,
-                    2,
+                    0,
                     Shape::rand(),
                     Color::rand()));
         }
@@ -72,21 +74,33 @@ pub mod board {
                 }
                 if self.compatible(&clone) {
                     self.block = Some(clone);
+                    true
+                } else {
+                    false
                 }
+            } else {
+                false 
             }
-            true
         }
 		fn compatible(&self, p: &Piece) -> bool {
 			p.cells.iter().all(
 				|&Cell{x,y,..}|
 				p.x+x < WIDTH  &&
 				p.y+y < HEIGHT &&
-				self.get(x,y).is_none())
+				self.get(p.x+x,p.y+y).is_none())
 		}
         fn incorporate(&mut self, p: &Piece) {
             for &c in p.cells.iter() {
                 self.table[p.y+c.y][p.x+c.x] = Some(c);
             }
+        }
+        fn incorporate2(&mut self) {
+            if let Some(ref b) = self.block {
+                for &c in b.cells.iter() {
+                    self.table[b.y+c.y][b.x+c.x] = Some(c);
+                }
+            }
+            self.block = None;
         }
         #[allow(dead_code)]
         pub fn random() -> Board {
@@ -111,7 +125,40 @@ pub mod board {
             }
             b
         }
+        pub fn command(&mut self, command: i32) {
+            //ncurses::getch() returns i32
+            use board::block::cell::Direction::*;
+            use ncurses::*;
+            let _success = match command {
+                //arros keys OR wasd OR hjkl
+                 97|104|KEY_LEFT    => self.shift(Left), 
+                100|108|KEY_RIGHT   => self.shift(Right),
+                115|106|KEY_DOWN    => self.shift(Down),
+                119|107|KEY_UP      => self.shift(Counterclockwise),
+                _                   => false,
+            };
+        }
+        pub fn update(&mut self) {
+            //shift down and check for complete lines 
+            if self.shift(Direction::Down) == false {
+                //block couldn't shift down
+                //add it to the board and create a new block
+                self.incorporate2();
+                self.new_piece();
+                //start checking for full lines (from bottom?)
+                for line in self.table.iter().rev() {
+                    if line.iter().all(|x| x.is_some()) {
+                        //full line
+                        println!("full line");
+                    }
+
+                }
+                
+            }
+                
+        }
         //for testing:
+        /*
         pub fn shift_left(&mut self) {
             self.shift(Direction::Left);
         }
@@ -120,7 +167,7 @@ pub mod board {
         }
         pub fn shift_down(&mut self) {
             self.shift(Direction::Down);
-        }
+        }*/
 
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 mod board;
 use board::board::*;
 extern crate rand; 
+extern crate ncurses;
 
 fn main() {
     //let b = board::board::new();
@@ -12,27 +13,12 @@ fn main() {
     //let mut p = board::block::Piece::new(5,5,);
 
     let mut b = Board::new();
-    //println!("{:?}", p);
-    //p.rotate_counterclockwise();
-    //b.incorporate(&p);
-    //println!("{}", b);
-    //
     b.new_piece();
-    b.shift_left();
-    b.shift_left();
-    b.shift_left();
-    b.shift_left();
-    loop {
-        //println!("{:?}", b.block);
-        //b.rotate3();
-        //b.shift2(1,2);
-        //b.shift2(board::cell::Direction::Right);
-        //b.shift_left();
-        b.shift_right();
-        b.shift_down();
 
-        //println!("{:?}", b.block);
+    loop {
+        b.update();
         println!("{}", b);
+        println!("\n\n\n\n\n");
         //std::thread::sleep_ms(500);
         let t = std::time::Duration::from_millis(200);
         std::thread::sleep(t);
